3253f0475d2046f605e93d0a880ae2072d5f0e9a
UntungJamari <untunggjamari@gmail.com>
2024-12-18 14:43:18 +0700

    update db

--- database/spkamanah.sql
+++ database/spkamanah.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 08, 2024 at 08:17 AM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 7.4.33
+-- Generation Time: Dec 18, 2024 at 08:41 AM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -36,7 +36,7 @@ CREATE TABLE `tbl_alternatif` (
   `tanggal_mulai_ditahan` date NOT NULL,
   `id_blok` int(11) NOT NULL,
   `id_periode` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tbl_alternatif`
@@ -44,6 +44,7 @@ CREATE TABLE `tbl_alternatif` (
 
 INSERT INTO `tbl_alternatif` (`id_alternatif`, `nama`, `tanggal_lahir`, `jenis_kelamin`, `jenis_kejahatan`, `tanggal_mulai_ditahan`, `id_blok`, `id_periode`) VALUES
 ('A.1', 'kevin', '2014-02-14', 'Laki-Laki', 'Kesusilaan', '2024-11-12', 1, 19),
+('A.2', 'Rapi', '2000-12-10', 'Laki-Laki', 'Kekerasan dalam Rumah Tangga', '2020-12-07', 1, 19),
 ('B.I-124/2022', 'Adek Irwan', '1983-11-09', 'Laki-Laki', 'Narkotika', '2021-05-29', 1, 12),
 ('B.I-124/2024', 'Fauzan', '2012-06-05', 'Laki-Laki', 'Kekerasan dalam Rumah Tangga', '2024-11-05', 1, 12),
 ('B.I-146/2022', 'Beni Afrianto', '1993-11-03', 'Laki-Laki', 'Narkotika', '2021-09-09', 1, 12),
@@ -53,8 +54,7 @@ INSERT INTO `tbl_alternatif` (`id_alternatif`, `nama`, `tanggal_lahir`, `jenis_k
 ('B.I-196/2023', 'Aditya Nurhidayat', '1991-07-20', 'Laki-Laki', 'Perlindungan Anak', '2022-08-02', 1, 12),
 ('B.I-203/2022', 'Diky Wahyudi', '1988-09-01', 'Laki-Laki', 'Perampokan', '2022-10-22', 1, 12),
 ('B.I-372/2022', 'Sepdi Herman', '1998-06-19', 'Laki-Laki', 'Perlindungan Anak', '2022-11-11', 1, 12),
-('B.I-764/2022', 'Dodi Linastra', '2002-02-28', 'Laki-Laki', 'Narkotika', '2022-05-31', 1, 12),
-('B.I-82/2023', 'Raplis', '1983-07-09', 'Laki-Laki', 'Penggelapan', '2022-08-03', 1, 12);
+('B.I-764/2022', 'Dodi Linastra', '2002-02-28', 'Laki-Laki', 'Narkotika', '2022-05-31', 1, 12);
 
 -- --------------------------------------------------------
 
@@ -65,7 +65,7 @@ INSERT INTO `tbl_alternatif` (`id_alternatif`, `nama`, `tanggal_lahir`, `jenis_k
 CREATE TABLE `tbl_blok` (
   `id` int(11) NOT NULL,
   `blok` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tbl_blok`
@@ -89,25 +89,18 @@ CREATE TABLE `tbl_hasil` (
   `id_periode` int(11) NOT NULL,
   `hasil` double NOT NULL,
   `persetujuan` varchar(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tbl_hasil`
 --
 
 INSERT INTO `tbl_hasil` (`id_hasil`, `id_alternatif`, `id_periode`, `hasil`, `persetujuan`) VALUES
-(17, 'B.I-124/2022', 12, 0.5, '1'),
-(18, 'B.I-146/2022', 12, 0.5, '1'),
-(19, 'B.I-146/2023', 12, 0.5, '1'),
-(20, 'B.I-152/2021', 12, 0.5, '1'),
-(21, 'B.I-159/2023', 12, 0.5, '0'),
-(22, 'B.I-196/2023', 12, 0.5, '1'),
-(23, 'B.I-203/2022', 12, 0.5, '0'),
-(24, 'B.I-372/2022', 12, 0.5, '0'),
-(25, 'B.I-764/2022', 12, 0.5, '1'),
-(26, 'B.I-82/2023', 12, 0.5, '0'),
-(39, 'B.I-124/2024', 12, 0.5, '0'),
-(40, 'A.1', 19, 1, '0');
+(44, 'A.1', 19, 0.2, '0'),
+(45, 'A.2', 19, 1, '0'),
+(46, 'B.I-124/2022', 12, 0.244741596, '0'),
+(47, 'B.I-124/2024', 12, 1, '0'),
+(48, 'B.I-146/2022', 12, 0.720614939, '0');
 
 -- --------------------------------------------------------
 
@@ -117,20 +110,27 @@ INSERT INTO `tbl_hasil` (`id_hasil`, `id_alternatif`, `id_periode`, `hasil`, `pe
 
 CREATE TABLE `tbl_kriteria` (
   `id_kriteria` int(11) NOT NULL,
+  `id_periode` int(11) NOT NULL,
   `nama_kriteria` varchar(255) NOT NULL,
   `jenis_kriteria` varchar(255) NOT NULL,
   `bobot` double(4,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tbl_kriteria`
 --
 
-INSERT INTO `tbl_kriteria` (`id_kriteria`, `nama_kriteria`, `jenis_kriteria`, `bobot`) VALUES
-(19, 'sudah menjalani hukuman selama 6 bulan', 'Benefit', 0.30),
-(20, 'berkelakuan baik', 'Benefit', 0.50),
-(21, 'sudah membayar denda', 'Benefit', 0.20),
-(22, 'p', 'Benefit', 0.50);
+INSERT INTO `tbl_kriteria` (`id_kriteria`, `id_periode`, `nama_kriteria`, `jenis_kriteria`, `bobot`) VALUES
+(21, 12, 'sudah membayar denda', 'Benefit', 0.05),
+(22, 12, 'p', 'Benefit', 0.40),
+(26, 12, 'a', 'Benefit', 0.30),
+(27, 12, 'ww', 'Benefit', 0.15),
+(29, 19, 'k19-1', 'Benefit', 0.30),
+(30, 19, 'k19-2', 'Benefit', 0.05),
+(31, 19, 'k19-3', 'Benefit', 0.05),
+(32, 19, 'k19-4', 'Benefit', 0.30),
+(33, 19, 'k19-5', 'Benefit', 0.30),
+(34, 12, 'qq', 'Benefit', 0.10);
 
 -- --------------------------------------------------------
 
@@ -143,73 +143,38 @@ CREATE TABLE `tbl_penilaian` (
   `id_alternatif` varchar(15) NOT NULL,
   `id_subkriteria` int(11) NOT NULL,
   `id_periode` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tbl_penilaian`
 --
 
 INSERT INTO `tbl_penilaian` (`id_penilaian`, `id_alternatif`, `id_subkriteria`, `id_periode`) VALUES
-(96, 'B.I-196/2023', 27, 12),
-(97, 'B.I-196/2023', 31, 12),
-(98, 'B.I-196/2023', 36, 12),
-(99, 'B.I-196/2023', 40, 12),
-(100, 'B.I-196/2023', 43, 12),
-(101, 'B.I-146/2023', 27, 12),
-(102, 'B.I-146/2023', 32, 12),
-(103, 'B.I-146/2023', 36, 12),
-(104, 'B.I-146/2023', 39, 12),
-(105, 'B.I-146/2023', 43, 12),
-(106, 'B.I-146/2022', 28, 12),
-(107, 'B.I-146/2022', 32, 12),
-(108, 'B.I-146/2022', 36, 12),
-(109, 'B.I-146/2022', 39, 12),
-(110, 'B.I-146/2022', 44, 12),
-(111, 'B.I-764/2022', 28, 12),
-(112, 'B.I-764/2022', 32, 12),
-(113, 'B.I-764/2022', 36, 12),
-(114, 'B.I-764/2022', 40, 12),
-(115, 'B.I-764/2022', 42, 12),
-(116, 'B.I-82/2023', 28, 12),
-(117, 'B.I-82/2023', 31, 12),
-(118, 'B.I-82/2023', 36, 12),
-(119, 'B.I-82/2023', 40, 12),
-(120, 'B.I-82/2023', 43, 12),
-(121, 'B.I-124/2022', 26, 12),
-(122, 'B.I-124/2022', 30, 12),
-(123, 'B.I-124/2022', 34, 12),
-(124, 'B.I-124/2022', 39, 12),
-(125, 'B.I-124/2022', 42, 12),
-(126, 'B.I-152/2021', 26, 12),
-(127, 'B.I-152/2021', 31, 12),
-(128, 'B.I-152/2021', 33, 12),
-(129, 'B.I-152/2021', 38, 12),
-(130, 'B.I-152/2021', 42, 12),
-(131, 'B.I-159/2023', 27, 12),
-(132, 'B.I-159/2023', 30, 12),
-(133, 'B.I-159/2023', 33, 12),
-(134, 'B.I-159/2023', 38, 12),
-(135, 'B.I-159/2023', 43, 12),
-(136, 'B.I-203/2022', 26, 12),
-(137, 'B.I-203/2022', 30, 12),
-(138, 'B.I-203/2022', 33, 12),
-(139, 'B.I-203/2022', 38, 12),
-(140, 'B.I-203/2022', 42, 12),
-(141, 'B.I-372/2022', 26, 12),
-(142, 'B.I-372/2022', 30, 12),
-(143, 'B.I-372/2022', 33, 12),
-(144, 'B.I-372/2022', 38, 12),
-(145, 'B.I-372/2022', 43, 12),
-(201, 'B.I-124/2024', 28, 12),
-(202, 'B.I-124/2024', 32, 12),
-(203, 'B.I-124/2024', 36, 12),
-(204, 'B.I-124/2024', 40, 12),
-(205, 'B.I-124/2024', 44, 12),
-(206, 'A.1', 28, 19),
-(207, 'A.1', 31, 19),
-(208, 'A.1', 36, 19),
-(209, 'A.1', 40, 19),
-(210, 'A.1', 44, 19);
+(224, 'A.1', 73, 19),
+(225, 'A.1', 79, 19),
+(226, 'A.1', 84, 19),
+(227, 'A.1', 89, 19),
+(228, 'A.1', 94, 19),
+(229, 'A.2', 69, 19),
+(230, 'A.2', 74, 19),
+(231, 'A.2', 80, 19),
+(232, 'A.2', 85, 19),
+(233, 'A.2', 90, 19),
+(234, 'B.I-124/2024', 48, 12),
+(235, 'B.I-124/2024', 57, 12),
+(236, 'B.I-124/2024', 61, 12),
+(237, 'B.I-124/2024', 65, 12),
+(238, 'B.I-124/2024', 95, 12),
+(239, 'B.I-124/2022', 51, 12),
+(240, 'B.I-124/2022', 60, 12),
+(241, 'B.I-124/2022', 64, 12),
+(242, 'B.I-124/2022', 68, 12),
+(243, 'B.I-124/2022', 99, 12),
+(244, 'B.I-146/2022', 50, 12),
+(245, 'B.I-146/2022', 58, 12),
+(246, 'B.I-146/2022', 62, 12),
+(247, 'B.I-146/2022', 66, 12),
+(248, 'B.I-146/2022', 97, 12);
 
 -- --------------------------------------------------------
 
@@ -220,7 +185,7 @@ INSERT INTO `tbl_penilaian` (`id_penilaian`, `id_alternatif`, `id_subkriteria`, 
 CREATE TABLE `tbl_periode` (
   `id_periode` int(11) NOT NULL,
   `keterangan` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tbl_periode`
@@ -242,17 +207,59 @@ CREATE TABLE `tbl_subkriteria` (
   `id_kriteria` int(11) NOT NULL,
   `ket` varchar(255) NOT NULL,
   `nbobot` double(4,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tbl_subkriteria`
 --
 
 INSERT INTO `tbl_subkriteria` (`id_subkriteria`, `id_kriteria`, `ket`, `nbobot`) VALUES
-(45, 19, 'sudah', 1.00),
-(46, 19, 'belum', 0.00),
-(47, 19, 'ff', 0.50),
-(48, 21, 'sudahh', 1.00);
+(48, 21, 'sudah', 1.00),
+(49, 21, 'sedang', 0.75),
+(50, 21, 'akan', 0.50),
+(51, 21, 'belum', 0.25),
+(57, 22, 'p1', 1.00),
+(58, 22, 'p2', 0.75),
+(59, 22, 'p3', 0.50),
+(60, 22, 'p4', 0.25),
+(61, 26, 'a1', 1.00),
+(62, 26, 'a2', 0.75),
+(63, 26, 'a3', 0.50),
+(64, 26, 'a4', 0.25),
+(65, 27, 'ww1', 1.00),
+(66, 27, 'ww2', 0.75),
+(67, 27, 'ww3', 0.50),
+(68, 27, 'ww4', 0.25),
+(69, 29, '1a', 1.00),
+(70, 29, '1b', 0.80),
+(71, 29, '1c', 0.60),
+(72, 29, '1d', 0.40),
+(73, 29, '1e', 0.20),
+(74, 30, '2a', 1.00),
+(75, 30, '2b', 0.80),
+(77, 30, '2c', 0.60),
+(78, 30, '2d', 0.40),
+(79, 30, '2e', 0.20),
+(80, 31, '3a', 1.00),
+(81, 31, '3b', 0.80),
+(82, 31, '3c', 0.60),
+(83, 31, '3d', 0.40),
+(84, 31, '3e', 0.20),
+(85, 32, '4a', 1.00),
+(86, 32, '4b', 0.80),
+(87, 32, '4c', 0.60),
+(88, 32, '4d', 0.40),
+(89, 32, '4e', 0.20),
+(90, 33, '5a', 1.00),
+(91, 33, '5b', 0.80),
+(92, 33, '5c', 0.60),
+(93, 33, '5d', 0.40),
+(94, 33, '5e', 0.20),
+(95, 34, 'qq1', 1.00),
+(96, 34, 'qq2', 0.80),
+(97, 34, 'qq3', 0.60),
+(98, 34, 'qq4', 0.40),
+(99, 34, 'qq5', 0.20);
 
 -- --------------------------------------------------------
 
@@ -267,7 +274,7 @@ CREATE TABLE `tbl_user` (
   `level` varchar(255) NOT NULL,
   `nama_lengkap` varchar(255) NOT NULL,
   `id_blok` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tbl_user`
@@ -343,19 +350,19 @@ ALTER TABLE `tbl_user`
 -- AUTO_INCREMENT for table `tbl_hasil`
 --
 ALTER TABLE `tbl_hasil`
-  MODIFY `id_hasil` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+  MODIFY `id_hasil` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 
 --
 -- AUTO_INCREMENT for table `tbl_kriteria`
 --
 ALTER TABLE `tbl_kriteria`
-  MODIFY `id_kriteria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id_kriteria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT for table `tbl_penilaian`
 --
 ALTER TABLE `tbl_penilaian`
-  MODIFY `id_penilaian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=211;
+  MODIFY `id_penilaian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=249;
 
 --
 -- AUTO_INCREMENT for table `tbl_periode`
@@ -367,7 +374,7 @@ ALTER TABLE `tbl_periode`
 -- AUTO_INCREMENT for table `tbl_subkriteria`
 --
 ALTER TABLE `tbl_subkriteria`
-  MODIFY `id_subkriteria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+  MODIFY `id_subkriteria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100;
 
 --
 -- AUTO_INCREMENT for table `tbl_user`
